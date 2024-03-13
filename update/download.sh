82b06a480929d7af3caf6cd31e3df33e702717b6
NVTA <162694616+nvta1209@users.noreply.github.com>
2024-03-13 14:19:10 -0700

    add inputs and outputs to update.sh steps

--- update/download.sh
+++ update/download.sh
@@ -6,6 +6,7 @@ FILE_DIR=$1
 FILES=(
     'gene/DATA/GENE_INFO/Mammalia/Homo_sapiens.gene_info.gz'
     'gene/DATA/gene2accession.gz'
+    'refseq/H_sapiens/mRNA_Prot/human.1.rna.gbff.gz'
 )
 
 for FILE in "${FILES[@]}"
--- update/update.sh
+++ update/update.sh
@@ -6,15 +6,22 @@ set -e
 LOCAL_NCBI_DIR=$(pwd)/ncbi
 PREVIOUS_UTA_VERSION=uta_20210129b
 
-# Download files for SeqRepo and UTA updates
+# 1. Download files for SeqRepo and UTA updates
+# Input: N/A
+# Output: LOCAL_NCBI_DIR populated with downloaded files
 mkdir -p $LOCAL_NCBI_DIR
 docker build --file Dockerfile.download --tag ncbi:latest --progress plain .
-docker run -it --mount type=bind,source=$LOCAL_NCBI_DIR,target=/data/ncbi ncbi:latest
+# docker run -it --mount type=bind,source=$LOCAL_NCBI_DIR,target=/data/ncbi ncbi:latest
 
-# Update SeqRepo
+# 2. Update SeqRepo
+# Input: TBD
+# Output: TBD
 # docker run seqrepo:latest -- seqrepo load
 
-# Update UTA, which depends on an updated SeqRepo
+# 3. Update UTA, which depends on an updated SeqRepo
+# Input: LOCAL_NCBI_DIR populated with downloaded files
+# Input: PREVIOUS_UTA_VERSION
+# Output: Postgres dump of updated database
 docker build --file ../Dockerfile --tag uta-update:latest ..
 docker build --file ../misc/docker/uta.dockerfile --tag uta:$PREVIOUS_UTA_VERSION --build-arg uta_version=$PREVIOUS_UTA_VERSION ../misc/docker
 previous_uta_version=$PREVIOUS_UTA_VERSION docker compose -f docker-compose.uta.yml run --rm uta-update
