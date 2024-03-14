40c550ac3e474aca9e00ce85531d5df3408ffa90
NVTA <162694616+nvta1209@users.noreply.github.com>
2024-03-13 17:51:08 -0700

    renumber steps

--- update/update.sh
+++ update/update.sh
@@ -13,18 +13,20 @@ mkdir -p $LOCAL_NCBI_DIR
 docker build --file Dockerfile.download --tag ncbi:latest --progress plain .
 # docker run -it --mount type=bind,source=$LOCAL_NCBI_DIR,target=/data/ncbi ncbi:latest
 
-# 2. Update SeqRepo
+# 2. Prepare SeqRepo
 # Input: TBD
 # Output: TBD
-# docker run seqrepo:latest -- seqrepo load
 
-# 3. Update UTA, which depends on an updated SeqRepo
+# 3. Prepare UTA
+docker build --file ../misc/docker/uta.dockerfile --tag uta:$PREVIOUS_UTA_VERSION --build-arg uta_version=$PREVIOUS_UTA_VERSION ../misc/docker
+
+# 4. Update UTA and SeqRepo
 # Input: LOCAL_NCBI_DIR populated with downloaded files
 # Input: PREVIOUS_UTA_VERSION
-# Output: Postgres dump of updated database
+# Output: Updated SeqRepo (updated in place)
+# Output: Updated UTA as a database dump
 docker build --file ../Dockerfile --tag uta-update:latest ..
-docker build --file ../misc/docker/uta.dockerfile --tag uta:$PREVIOUS_UTA_VERSION --build-arg uta_version=$PREVIOUS_UTA_VERSION ../misc/docker
 previous_uta_version=$PREVIOUS_UTA_VERSION docker compose -f docker-compose.uta.yml run --rm uta-update
-
-# Stop dependency containers
 docker compose -f docker-compose.uta.yml down
+
+# 5. Publish UTA and SeqRepo
